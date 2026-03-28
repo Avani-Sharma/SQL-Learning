bee33a9fba239830d65ae29089169fb14aa5cb0e
Avani Sharma <avanisharma2405@gmail.com>
2026-03-28 20:12:53 +0530

    Add files via upload

--- Basics and Where Clause.sql
+++ Basics and Where Clause.sql
@@ -61,3 +61,106 @@ SELECT * FROM country WHERE Continent IN ('Asia', 'Africa');
 
 -- Ques9.--> Countries not Asia or Africa
 SELECT * FROM country WHERE Continent not IN ('Asia', 'Africa');
+
+
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(50),
+    age INT,
+    marks INT,
+    city VARCHAR(50)
+);
+
+INSERT INTO students (id, name, age, marks, city) VALUES
+(1, 'Avani', 20, 85, 'Jaipur'),
+(2, 'Rohit', 22, 72, 'Delhi'),
+(3, 'Sneha', 19, 90, 'Mumbai'),
+(4, 'Amit', 21, 65, 'Pune'),
+(5, 'Priya', 20, 78, 'Jaipur'),
+(6, 'Karan', 23, 55, 'Delhi'),
+(7, 'Neha', 18, 88, 'Chandigarh'),
+(8, 'Rahul', 22, 40, 'Bangalore'),
+(9, 'Simran', 21, 95, 'Amritsar'),
+(10, 'Vikas', 24, 60, 'Lucknow');
+
+
+-- practice questions based on table student:-- 
+-- Fetch all students whose marks are greater than 50 
+select name from students where marks> 50;
+
+-- Display students whose age is 18
+select name from students where age = 18;
+
+-- Get students whose city is 'Delhi'
+select name from students where city = 'Delhi';
+
+-- Find students whose marks are between 40 and 80
+select name from students where marks between 40 and 80;
+
+-- Fetch students whose marks are less than 60 AND city is 'Mumbai'
+select name from students where marks <60 and city = 'Mumbai';
+
+-- Find students whose name starts with 'A'
+select name from students where name like 'A%';
+
+-- Find students whose name ends with 'a'
+select name from students where name like '%a';
+
+-- Get students whose age is 18 OR 20
+select name from students where age in (18, 20);
+
+-- Fetch students whose marks are 50, 60, or 70
+select name from students where marks in (50, 60, 70); 
+
+-- Find students whose city is NULL
+select name from students where city is Null;
+
+CREATE TABLE employees (
+    emp_id INT,
+    emp_name VARCHAR(50),
+    salary INT,
+    department VARCHAR(50),
+    experience INT
+);
+
+INSERT INTO employees (emp_id, emp_name, salary, department, experience) VALUES
+(1, 'Amit', 30000, 'HR', 2),
+(2, 'Neha', 45000, 'IT', 4),
+(3, 'Rahul', 50000, 'Finance', 5),
+(4, 'Priya', 35000, 'HR', 3),
+(5, 'Karan', 60000, 'IT', 6),
+(6, 'Sneha', 40000, 'Marketing', 4),
+(7, 'Vikas', 55000, 'Finance', 5),
+(8, 'Simran', 32000, 'Marketing', 2),
+(9, 'Ankit', 70000, 'IT', 8),
+(10, 'Rohit', 28000, 'HR', 1);
+
+-- Fetch employees whose salary is greater than 30000
+select emp_name from employees where salary > 30000;
+
+-- Get employees whose department is 'IT'
+select emp_name from employees where department =  'IT';
+
+-- Find employees whose salary is between 20000 and 50000
+select emp_name from employees where salary >= 20000 AND salary <= 50000;
+
+-- Fetch employees whose experience is less than 5 years
+select emp_name from employees where experience < 5;
+
+-- Find employees whose name starts with 'R'
+select emp_name from employees where emp_name like 'R%';
+
+-- Get employees whose department is 'HR' OR 'Finance'
+select emp_name from employees where department in ('HR', 'Finance');
+
+-- Fetch employees whose salary is not equal to 40000
+select emp_name from employees where salary <> 40000;
+
+-- Find employees whose experience is 3, 5, or 7
+select emp_name from employees where experience in (3, 5, 7);
+
+-- Get employees whose department is NULL
+select emp_name from employees where department is null;
+
+-- Fetch employees whose salary is less than 25000 OR greater than 60000
+select emp_name from employees where salary < 25000 or salary >60000;

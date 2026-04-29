2b9ce756f923f1cc7cd8ca72c558a47eba203ed1
Avani Sharma <avanisharma2405@gmail.com>
2026-04-29 18:46:21 +0530

    Add files via upload

--- order by clause.sql
+++ order by clause.sql
@@ -39,3 +39,32 @@ SELECT Continent, COUNT(Name) FROM country GROUP BY Continent ORDER BY country_c
 -- WHERE + GROUP BY + ORDER BY (Together)
 -- Display regions in Asia with average life expectancy, ordered by average life expectancy.
 SELECT Region, AVG(LifeExpectancy) FROM country WHERE Continent = 'Asia' GROUP BY Region ORDER BY avg_life DESC;
+
+
+select * from employees;
+select * from employees order by salary desc;
+
+select first_name, department, salary from employees order by department, salary;
+
+select salary, hire_date, year(hire_date) from employees order by year(hire_date) , salary desc;
+
+-- functions : multi-row functions/aggregate functions
+-- distinct: unique values
+select distinct(department) from employees;
+
+select distinct department, salary from employees;
+
+-- calculation on your data : aggregate function
+select count(department) from employees;
+select count(first_name) from employees;
+
+select count(district) from address;
+select count(district) , count(address_id) , count(address2) from address;
+
+select * from address;
+select * from employees;
+select count(department), count(distinct department) from employees;
+
+-- select salary, count(salary) from employees;   : error 
+select sum(salary), count(salary) from employees;
+select max(salary), min(salary), avg(salary) from employees;

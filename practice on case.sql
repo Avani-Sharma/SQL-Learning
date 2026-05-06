38ad631278ea2e17bd1aa4665d5d583caa48709b
Avani Sharma <avanisharma2405@gmail.com>
2026-05-06 11:52:33 +0530

    Add files via upload

--- practice on case.sql
+++ practice on case.sql
@@ -1,4 +1,4 @@
--- case:
+-- case: 
 /*
 if (condition , true, false)
 if (conditon, true , if (condition, true, if)
@@ -10,6 +10,75 @@ case:
    True --> 1 
    False --> 0
 */
+use sakila;
+select * from employees;
+select first_name , department, salary, if (department = 'IT' , True, False) from employees;
+select first_name, department, salary, if(department= 'IT', salary*1.5, salary*1.05) from employees;
+
+select first_name, department, salary, 
+if(department = 'IT', salary*1.1, if(department= 'Hr', salary*0.5, salary)) from employees;
+
+-- case statement
+/* 
+select col1, col2
+case
+    when condition then statement
+end 
+from table_name
+*/
+
+select first_name, department, salary, 
+case 
+    when department = 'IT' then True
+    else False
+end
+from employees;
+
+
+select first_name, department, salary, 
+case 
+    when department = 'IT' or department = 'HR'  then 'A category'
+    else 'B category'
+end
+from employees;
+
+select first_name, department, salary, 
+case 
+    when department = 'IT' then salary*1.1
+    when department = 'IT' then salary*1.05
+    else salary
+end as 'New salary'
+from employees;
+
+select * from employees;
+-- if the hiring date of an employee is before 2020 year print job_title with senior if 2021 print associate
+select first_name, job_title, year(hire_date),
+case
+    when year(hire_date) <2020  then CONCAT('senior ', job_title)
+    when year(hire_date) = 2021 then CONCAT('associate ', job_title)
+    else job_title
+end as title
+from employees;
+
+-- if salary >70000, create the category name as avg salary if the salary is > 85000 high salary 
+-- if salary >100000 then extreme high salary othewise print the category name as low salary
+SELECT first_name, salary,
+CASE
+    WHEN salary > 100000 THEN 'extreme high salary'
+    WHEN salary > 85000 THEN 'high salary'
+    WHEN salary > 70000 THEN 'avg salary'
+    ELSE 'low salary'
+END AS update_slry
+FROM employees;
+
+-- select the job_title and print the column value based on the following cond 
+SELECT job_title,
+       CASE 
+           WHEN COUNT(*) = 1 THEN CONCAT(job_title, ' has 1 person')
+           ELSE CONCAT(job_title, ' has ', COUNT(*), ' persons')
+       END AS job_info
+FROM employees
+GROUP BY job_title;
 
 use world;
 
@@ -58,4 +127,3 @@ sum(case
     else 0 
     end )
 from world.country
-group by continent;

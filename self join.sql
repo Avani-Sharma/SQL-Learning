e257ccca8495b3e4edd54f1eae30c00950638f85
Avani Sharma <avanisharma2405@gmail.com>
2026-05-08 13:19:52 +0530

    Add files via upload

--- self join.sql
+++ self join.sql
@@ -41,3 +41,67 @@ where emp.manager_id = manager.emp_id;
 select emp.emp_id, emp.emp_name, emp.manager_id, emp.salary, manager.emp_id, manager.emp_name , manager.salary
 from employee as emp join employee as manager
 where emp.manager_id = manager.emp_id;
+
+
+-- Self join: a table when joined with itself 
+create database joindb;
+
+use joindb;
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department VARCHAR(50),
+    manager_id INT,
+    salary INT
+);
+
+INSERT INTO employees (emp_id, emp_name, department, manager_id, salary) VALUES
+(1, 'Amit', 'Management', NULL, 120000),
+(2, 'Neha', 'HR', 1, 75000),
+(3, 'Raj', 'IT', 1, 90000),
+(4, 'Simran', 'Finance', 1, 85000),
+(5, 'Karan', 'IT', 3, 70000),
+(6, 'Priya', 'IT', 3, 72000),
+(7, 'Rohit', 'HR', 2, 50000),
+(8, 'Anjali', 'HR', 2, 52000),
+(9, 'Vikas', 'Finance', 4, 65000),
+(10, 'Sneha', 'Finance', 4, 62000),
+(11, 'Arjun', 'IT', 5, 55000),
+(12, 'Pooja', 'IT', 5, 53000),
+(13, 'Meera', 'HR', 7, 45000),
+(14, 'Dev', 'Finance', 9, 48000),
+(15, 'Nitin', 'IT', 6, 51000);
+
+
+
+/*
+
+Amit
+ ├── Neha
+ │    ├── Rohit
+ │    │     └── Meera
+ │    └── Anjali
+ │
+ ├── Raj
+ │    ├── Karan
+ │    │     ├── Arjun
+ │    │     └── Pooja
+ │    └── Priya
+ │           └── Nitin
+ │
+ └── Simran
+      ├── Vikas
+      │     └── Dev
+      └── Sneha
+
+*/
+
+select * from employees;
+
+select empl.emp_id, empl.emp_name, empl.manager_id , mngr.emp_id, mngr.emp_name 
+from employees as empl
+join employees as mngr
+where empl.manager_id = mngr.emp_id;
+
+
